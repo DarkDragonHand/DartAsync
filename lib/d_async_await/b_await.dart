6c87ac77d8e5a3ec653a3d272273c20c66d2c6e8
Guilherme Potter Petry <guipotterrp@gmail.com>
2023-06-25 17:14:20 -0300

    aula Modificador Await concluída

--- lib/d_async_await/b_await.dart
+++ lib/d_async_await/b_await.dart
@@ -1,3 +1,14 @@
 void main() {
-  
+  final totalAlunos = buscarTotalDeAlunos();
+  totalAlunos.then((totalAlunosValue) {
+    print('O total de alunos da ADF é: $totalAlunosValue');
+  });
+  print('Resposta: $totalAlunos');
+}
+
+Future<int> buscarTotalDeAlunos() async {
+  print('Esse ponto da função é sincrono');
+  final buscandoTotalDeAlunos = await Future.value(1000);
+  print('Esse ponto da função é depois do Await');
+  return buscandoTotalDeAlunos;
 }
