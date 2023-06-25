92c4a16d0ee2579aafd66774662f702d0dce0f47
Guilherme Potter Petry <guipotterrp@gmail.com>
2023-06-25 19:27:03 -0300

    aula Registrando um tempo para execução de um Future concluída

---
+++ lib/d_async_await/f_timeout.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+//!Importante
+//*Timeout é o tempo limite para execução de um Future
+//!O Timeout não cancela a ação do Future
+//!Ele vai continuar fazendo a ação mesmo com timeout, até terminar.
+Future<void> main() async {
+  /*final aguardando = inserindoAlgoBemLento();
+  print(DateTime.now().toIso8601String());
+  aguardando.timeout(
+    Duration(seconds: 1),
+    onTimeout: () {
+      print('Terminou com Timeout');
+    },
+  ).then((value) {
+    print('Terminou a execução');
+  }).catchError((erro) {
+    print('Terminou com erro: $erro');
+  });*/
+
+  try {
+    final aguardando = await inserindoAlgoBemLento().timeout(Duration(seconds: 1));
+  } on TimeoutException catch (e) {
+    print('Finalizou com TimeOut');
+  }
+}
+
+Future<void> inserindoAlgoBemLento() {
+  return Future.delayed(Duration(seconds: 3), () {
+    print('Terminou o insert');
+    print(DateTime.now().toIso8601String());
+  });
+}
